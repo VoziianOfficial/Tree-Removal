eec67e7e5b4cee444d381f53ea275e31ee3d7bfe
Yuliia Voziian <vozianulia@gmail.com>
2026-08-27 11:20:47 +0200

    !

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 window.SiteConfig = {
   companyName: "TimberRoot Tree Removal",
-  logo: "assets/icons/logo.svg",
-  favicon: "assets/icons/favicon.svg",
+  logo: "assets/icons/logo.png",
+  favicon: "assets/icons/favicon.png",
   email: "hello@timberroot.example",
   disclaimer: "Independent tree removal service aggregator. Service availability, credentials, insurance, and final estimates should be confirmed directly before booking.",
   browserTitle: "TimberRoot Tree Removal | Tree Services"
